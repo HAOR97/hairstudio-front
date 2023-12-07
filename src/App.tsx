0699a52547fab7707055e9d089dde8cad373f7a4
HAOR97 <134957173+HAOR97@users.noreply.github.com>
2023-12-07 21:04:58 +0100

    add comentar

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import HomeAdmin from "./pages/adminPages/HomeAdmin.tsx";
 import HomeBarber from "./pages/berberPages/HomeBarber.tsx";
 import { Navigate } from "react-router-dom";
 //dodat je komentar u githubu
+//dodato iz lokalnog repozitoriuma
 
 function App() {
   const loggedIn = localStorage.getItem("isLoggedIn")
